5f639b6c02c00c05705a1dba3d6bc6f642b08ea1
Zarko Stojanov <stojanov1987@gmail.com>
2024-04-23 00:44:18 +0200

    - added Home.js

---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+import allVideos from '../Components/allVideos'
+const Home = () => {
+    return(
+        <>
+            <h1>Glavna Stranica</h1>
+        <allVideos />
+        </>
+    )
+}
+export default Home
